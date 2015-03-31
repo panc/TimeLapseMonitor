70e1c474434db6e7ab8e23a2e0494c25dbcb0ff0
Christoph Pangerl <christoph.pangerl@gmail.com>
2015-03-31 23:30:57 +0200

    Removed server side sails stuff and use a pure and thus very simple express implementation for the server side!

--- app.js
+++ app.js
@@ -1,59 +1,44 @@
-/**
- * app.js
- *
- * Use `app.js` to run your app without `sails lift`.
- * To start the server, run: `node app.js`.
- *
- * This is handy in situations where the sails CLI is not relevant or useful.
- *
- * For example:
- *   => `node app.js`
- *   => `forever start app.js`
- *   => `node debug app.js`
- *   => `modulus deploy`
- *   => `heroku scale`
- *
- *
- * The same command-line arguments are supported, e.g.:
- * `node app.js --silent --port=80 --prod`
- */
+var express = require('express');
+var app = express();
 
-// Ensure we're in the project directory, so relative paths work as expected
-// no matter where we actually lift from.
-process.chdir(__dirname);
+var fs = require('fs');
+var path = require('path');
 
-// Ensure a "sails" can be located:
-(function() {
-  var sails;
-  try {
-    sails = require('sails');
-  } catch (e) {
-    console.error('To run an app using `node app.js`, you usually need to have a version of `sails` installed in the same directory as your app.');
-    console.error('To do that, run `npm install sails`');
-    console.error('');
-    console.error('Alternatively, if you have sails installed globally (i.e. you did `npm install -g sails`), you can use `sails lift`.');
-    console.error('When you run `sails lift`, your app will still use a local `./node_modules/sails` dependency if it exists,');
-    console.error('but if it doesn\'t, the app will run with the global sails instead!');
-    return;
-  }
+var IMAGE_PATH = 'camera';
+var directory = path.join(process.cwd(), IMAGE_PATH);
 
-  // Try to get `rc` dependency
-  var rc;
-  try {
-    rc = require('rc');
-  } catch (e0) {
-    try {
-      rc = require('sails/node_modules/rc');
-    } catch (e1) {
-      console.error('Could not find dependency: `rc`.');
-      console.error('Your `.sailsrc` file(s) will be ignored.');
-      console.error('To resolve this, run:');
-      console.error('npm install rc --save');
-      rc = function () { return {}; };
-    }
-  }
+app.get('/images', function (req, res) {
+    var files = fs.readdirSync(directory)
+        .map(function (file) {
+            return {
+                name: file,
+                url: path.join(IMAGE_PATH, file)
+            };
+    });
+    
+    res.json(files);
+});
 
+app.get('/images/:image', function (req, res) {
+    var file = path.join(directory, req.params.image);
+    
+    fs.exists(file, function (exists) {
+        if (!exists)
+            return res.notFound(file);
+        
+        return fs.createReadStream(file).pipe(res);
+    });
+});
 
-  // Start server
-  sails.lift(rc('sails'));
-})();
+app.get('*', function (req, res) {
+    res.send('Hello World!');
+});
+
+var server = app.listen(3000, function () {
+    
+    var host = server.address().address;
+    var port = server.address().port;
+    
+    console.log('Example app listening at http://%s:%s', host, port);
+
+});
