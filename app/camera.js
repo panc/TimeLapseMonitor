8c32e97ed42d6f60fea702671798b64d28ebf198
Christoph Pangerl <christoph.pangerl@gmail.com>
2015-04-05 22:45:04 +0200

    Improved dummy implementations for windows.

--- app/camera.js
+++ app/camera.js
@@ -6,20 +6,27 @@ var IMAGE_PATH = 'camera';
 var imageDirectory = path.join(process.cwd(), IMAGE_PATH);
 
 module.exports = function () {
-
+    
     var platform = require('os').platform();
     var camera;
     var onNewPhotoCallback;
-
+    
     if (platform.indexOf("win") == 0) {
         console.log("Camera not supported on windows!");
-    
+        
+        var onExitCallback;
+        
         camera = {
-            start: function () { console.log('Camera started (but on windows not supported)'); },
+            start: function () {
+                console.log('Take dummy photo...');
+                onExitCallback();
+            },
+            on: function (key, callback) {
+                onExitCallback = callback;
+            },
             stop: function () { },
             get: function (opt) { },
-            set: function (opt, value) { },
-            on: function (key, callback) { }
+            set: function (opt, value) { }
         };
     }
     else {
@@ -32,7 +39,7 @@ module.exports = function () {
             quality: 100
         });
     }
-
+    
     camera.on("exit", function (err, timestamp, filename) {
         
         onAfterNewPhoto();
@@ -40,10 +47,10 @@ module.exports = function () {
     
     var takePhoto = function () {
         
-        var format = function(value) {
+        var format = function (value) {
             return ("0" + value).slice(-2);
         }
-
+        
         var time = new Date();
         var fileName = time.getFullYear() +
             format(time.getMonth()) + 
@@ -51,7 +58,7 @@ module.exports = function () {
             format(time.getHours()) + 
             format(time.getMinutes()) + 
             format(time.getSeconds());
-
+        
         camera.set('output', path.join(imageDirectory, fileName));
         camera.start();
     }
@@ -68,22 +75,25 @@ module.exports = function () {
                     url: path.join(IMAGE_PATH, file)
                 };
             });
-        
+            
             onNewPhotoCallback(files);
         });
     };
     
     return {
-        onNewPhoto: function(newPhotoCallback) {
+        onNewPhoto: function (newPhotoCallback) {
             onNewPhotoCallback = newPhotoCallback;
         },
-
+        
         takePhoto: takePhoto,
-
+        
         startTimelapse: function () {
-
+            
             console.log('Starting timelapse...');
-            setInterval(takePhoto, 10000);
+            
+            // Taking the photo takes about 5 to 10 seconds.
+            // This offset must be included in the interval time!
+            setInterval(takePhoto, 17 * 1000);
         }
     };
 };
